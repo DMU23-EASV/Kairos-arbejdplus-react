4c11b03d40bd067e70f0559813158d366b4d2517
Mads Knudsen <mads@rknudsen.dk>
2024-12-19 10:47:16 +0100

    Fix: Changed Title for Tasks

--- src/Constants.ts
+++ src/Constants.ts
@@ -10,4 +10,4 @@ export const SAVE_TASK_ENDPOINT: string = "";
 
 export const SEND_TASK_ENDPOINT: string = "http://localhost:8080/api/task/";
 
-export const NEW_TASK_PAGE_TITLE: string = "Ny tidsregistrering";
+export const NEW_TASK_PAGE_TITLE: string = "Tidsregistrering";
